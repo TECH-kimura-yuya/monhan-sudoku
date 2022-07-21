8d3ee1d6bbe5ff536934ac8387bf5a89c6e7bc06
Y.yutaka <yuttan1022@icloud.com>
2022-07-21 10:38:51 +0900

    mainを書き換えました。

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    
+char matrix[9][9];
+
+int x,y,z;
+
+void Reset()
+{
+  for(int x = 0; x<9; x++)
+    {
+      for(int y = 0; y<9; y++)
+        {
+          printf("%3d", matrix[x][y]);
+
+          if ((x==3)&&(y==9)||(x==6)&&(y==9))
+          {
+            printf("%*s", z, "");
+            cout << "\n" << endl;
+          }
+          
+        }
+      cout << endl;
+    }
+}
+
+int main() {
+  //for(int i=1;i==81;i++){
+    //int x;
+    //double num[i];
+    //if(i%9==0){
+      //cout << num[i] << endl;
+    //}else{
+      //cout << num[i];
+    //}
+  //}
+
+  Reset();
+  
 }
